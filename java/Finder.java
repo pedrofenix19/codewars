ef0d1351bee309438e1686fb395349a0381caa47
Pedro Deniz <pedeniz@gmail.com>
2019-02-22 08:40:40 +0100

    java

--- java/Finder.java
+++ java/Finder.java
@@ -1,28 +1,60 @@
+import java.awt.Point;
+import java.util.ArrayList;
+
 public class Finder {
 
-    private class Point{
-        int x;
-        int y;
+    static ArrayList<Point> getPossibleNextPositions(String[] mazearr, Point currentPosition){
+        ArrayList<Point> possibleNextPositions = new ArrayList<Point>();
+        int n = mazearr.length;
 
-        public Point(int x, int y){
-            this.x = x;
-            this.y = y;
+        //if can move down
+        if(currentPosition.x < n - 1 && mazearr[currentPosition.x + 1].charAt(currentPosition.y) != 'W'){
+            possibleNextPositions.add(new Point(currentPosition.x + 1, currentPosition.y));
         }
 
-        public String toString(){
-            return "(" + this.x + "," + this.y + ")";
+        //if can move right
+        if(currentPosition.y < n - 1 && mazearr[currentPosition.x].charAt(currentPosition.y + 1) != 'W'){
+            possibleNextPositions.add(new Point(currentPosition.x, currentPosition.y + 1));
         }
+
+        //if can move left
+        if(currentPosition.y > 0 && mazearr[currentPosition.x].charAt(currentPosition.y - 1) != 'W'){
+            possibleNextPositions.add(new Point(currentPosition.x, currentPosition.y - 1));
+        }
+
+        //if can move up
+        if(currentPosition.x > 0 && mazearr[currentPosition.x - 1].charAt(currentPosition.y) != 'W'){
+            possibleNextPositions.add(new Point(currentPosition.x - 1, currentPosition.y));
+        }
+
+        return possibleNextPositions;
     }
+
     
     static boolean pathFinder(String maze) {
-        // Your code here!!
+        String[] mazearr = maze.split("\n");
+        int n = mazearr.length;
+        ArrayList<Point> openPoints = new ArrayList<Point>();
+        openPoints.add(new Point(0,0));
+        ArrayList<Point> closedPoints = new ArrayList<Point>();
+
+        while (openPoints.size() > 0) {
+            Point currentPoint = openPoints.remove(0);
+            ArrayList<Point> possibleNextPoints = getPossibleNextPositions(mazearr, currentPoint);
+
+            for (Point p : possibleNextPoints) {
+                if(p.x == n -1 && p.y == n - 1) return true;
+            }
+
+            for (Point p : possibleNextPoints) {
+                if(!closedPoints.contains(p)) openPoints.add(p);
+            }
+            closedPoints.add(currentPoint);
+        }
         return false;
     }
 
     public static void main(String[] args){
-        Point hola = new Point(1,2);
-        System.out.println(hola);
-
         String a = ".W.\n"+
                  ".W.\n"+
                  "...",
